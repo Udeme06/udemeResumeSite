4af6179f7b164b495113483616aefb904ef0570b
Udeme06 <ekpoudemeusen@gmail.com>
2015-10-31 22:43:04 -0400

    Add action.js

---
+++ js/action.js
@@ -0,0 +1,7 @@
+function swap(one, two, three, four, five) {
+    document.getElementById(one).style.display = 'block';
+    document.getElementById(two).style.display = 'none';
+    document.getElementById(three).style.display = 'none';
+    document.getElementById(four).style.display = 'none';
+    document.getElementById(five).style.display = 'none';
+}
